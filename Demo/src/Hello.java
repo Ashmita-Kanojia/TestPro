231262e06f0b23f7eeed70dfbf44af2412d483b6
Ashmita-Kanojia <58498200+Ashmita-Kanojia@users.noreply.github.com>
2019-12-11 08:26:16 +0530

    Update Hello.java
    Updated Java File

--- Demo/src/Hello.java
+++ Demo/src/Hello.java
@@ -3,6 +3,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
+		int i = 2;
+		int j = 3;
+		int k = i+j;
+		System.out.println(k);
+		
 		System.out.println("HelloWorld");
 		System.out.println("Hello AShmita Kanojia");
 	}
