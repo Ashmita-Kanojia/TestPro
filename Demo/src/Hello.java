55e9d9f0e453af5b563fa516a9a47cf2c4960e9d
user <user@192.168.100.6>
2019-12-11 08:29:20 +0530

    Error

--- Demo/src/Hello.java
+++ Demo/src/Hello.java
@@ -3,7 +3,18 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("HelloWorld");
+		//System.out.println("HelloWorld");
 		System.out.println("Hello AShmita Kanojia");
+		try {
+		int i = 2/0;
+		
+		}
+		catch(Exception e) {
+			
+			//e.printStackTrace();
+			//System.out.println(e.toString());
+			System.out.println(e.getMessage());
+		}
+		//System.out.println(i);
 	}
 }
